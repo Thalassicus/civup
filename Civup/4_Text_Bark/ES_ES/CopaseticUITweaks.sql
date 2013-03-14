f1270c401aa9cd4268a4fdb38948e2d8e27b3f07
JairoAlonso25 <jairoalonso25@gmail.com>
2013-03-14 10:36:06 -0500

    Update CopaseticUITweaks.sql
    Fully translated.

--- Civup/4_Text_Bark/ES_ES/CopaseticUITweaks.sql
+++ Civup/4_Text_Bark/ES_ES/CopaseticUITweaks.sql
@@ -1,1 +1,5 @@
---·
+--· ES_ES/CopaseticUITweaks.sql
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_CITY_CONNECTED', 'Puede comerciar con la [ICON_CAPITAL] Capital por carretaras o rutas marítimas.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_CITY_CONNECTED_RAILROAD', 'Puede comerciar con la [ICON_CAPITAL] Capital por ferrocariles o rutas marítimas.', '', '');
+
+UPDATE LoadedFile SET Value=1 WHERE Type='Civup_CopaseticUITweaks.sql';
