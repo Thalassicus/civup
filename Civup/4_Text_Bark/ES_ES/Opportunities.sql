254987a9299100c7d3eb6d107a45d33b5a2a4e15
JairoAlonso25 <jairoalonso25@gmail.com>
2013-03-14 14:55:21 -0500

    Update Opportunities.sql
    Fully Translated

--- Civup/4_Text_Bark/ES_ES/Opportunities.sql
+++ Civup/4_Text_Bark/ES_ES/Opportunities.sql
@@ -1,1 +1,10 @@
---·
+--· ES_ES/Opportunities.sql
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_USER_EVENT_OPTION_SELECT', 'Seleccionar.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_USER_EVENT_OPTION_OK', 'Esta bien.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_DISTANT_PLAYER', 'un jugador insatisfecho.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_DISTANT_CITY', 'una ciudad distante.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_DISTANT_UNIT', 'unidad distante.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_TRIGGER_YIELD_COST', '{1_iconstring} Coste: {2_sign}{3_num}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-14'), 'TXT_KEY_TRIGGER_YIELD_PER_TURN', '{1_iconstring} {2_yieldname}: {3_sign}{4_yield}', '', '');
+
+UPDATE LoadedFile SET Value=1 WHERE Type='Civup_Opportunities.sql';
