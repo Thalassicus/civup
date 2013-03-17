3179d071e5df20049017dc5ccb8849db558568a2
JairoAlonso25 <jairoalonso25@gmail.com>
2013-03-17 17:43:36 -0500

    Update AutoTips_Tips.sql
    11% Translated.
    
    
    Sections: Units and Buildings fully transalted

--- Civup/4_Text_Bark/ES_ES/AutoTips_Tips.sql
+++ Civup/4_Text_Bark/ES_ES/AutoTips_Tips.sql
@@ -1,1 +1,28 @@
---·
+--· ES_ES/AutoTips_Tips.sql
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_TOOLTIP_GOOD_FOR', 'Bueno para:', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_TOOLTIP_ABILITIES', 'Habilidades:', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_TOOLTIP_REQUIREMENTS', 'Requerimientos:', '', '');
+/* Unidades */
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_MAINTENANCE', 'Mantenimiento [ICON_GOLD]: {1_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_HURRY_COST_MODIFIER', 'Comprar [ICON_GOLD]: {1_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_HURRY_COST_MODIFIER', 'Comprar [ICON_GOLD]: {3_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_FOUND', 'Funde ciudades para expandir su imperio [ICON_TRADE].', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_FOOD', 'Construya más rapido en ciudades de alto crecimiento [ICON_BULLET] cambiando alimento [ICON_FOOD] por producción [ICON_PRODUCTION].', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_WORK_RATE', 'Tasa de mejoramiento [ICON_WORKER]: {1_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_OBSOLETE_TECH', 'Obsoleto [ICON_PIRATE]: {1_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_CARGO', 'Transporta [ICON_AIR]: {1_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_SUICIDE', 'Un disparo [ICON_PIRATE].', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_PRODUCTION_UNIT_NUKE_RADIUS', ' [COLOR_POSITIVE_TEXT]Radio nuclear [ICON_RES_URANIUM]: {1_value}[ENDCOLOR]', '', '');
+/* Edificaciones */
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_COST', 'Coste [ICON_PRODUCTION]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_NUM_CITY_COST_MOD', 'Coste [ICON_PRODUCTION]: {2_sign}{3_value} Por cada ciudad.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_POP_COST_MOD', 'Coste [ICON_PRODUCTION] por habitante [ICON_CITIZEN]: {2_sign}{3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_GOLD_MAINTENANCE', 'Mantenimiento [ICON_GOLD]: {3_value}', '', '');
+/*  Yields  */
+
+/* Other */
+
+/* Added by Civup */
+
+/* Expansion */
+
