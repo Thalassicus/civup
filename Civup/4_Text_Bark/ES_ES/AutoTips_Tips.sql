f84ff3cf5ae724b206cc12e86d0a9a433dd283ff
Jairo Alonso Velásquez <jairoalonso25@gmail.com>
2013-03-19 21:00:22 -0500

    Update AutoTips_Tips.sql and Civup_Language.xls

--- Civup/4_Text_Bark/ES_ES/AutoTips_Tips.sql
+++ Civup/4_Text_Bark/ES_ES/AutoTips_Tips.sql
@@ -18,11 +18,57 @@ INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VA
 INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_NUM_CITY_COST_MOD', 'Coste [ICON_PRODUCTION]: {2_sign}{3_value} Por cada ciudad.', '', '');
 INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_POP_COST_MOD', 'Coste [ICON_PRODUCTION] por habitante [ICON_CITIZEN]: {2_sign}{3_value}', '', '');
 INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-17'), 'TXT_KEY_BUILDING_EFFECT_GOLD_MAINTENANCE', 'Mantenimiento [ICON_GOLD]: {3_value}', '', '');
-/*  Yields  */
-
+/*  Rendimientos  */
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_IN_ALL_CITIES', 'en todas las ciudades.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_MILITARY_UNITS', 'unidades militares.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_INSTANT', '{1_prefix} Inmediatamente: {2_sign}{3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_CHANGE', '{1_prefix}: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_CHANGE_FROM', '{1_prefix}: {3_value} {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD', '{1_prefix}: {2_sign}{3_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_FOR', '{1_prefix}: {2_sign}{3_value}% para {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_MILITARY', '{1_prefix}: {2_sign}{3_value}% para unidades militares.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_DOMAIN', '{1_prefix}: {2_sign}{3_value}% para {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_COMBAT', '{1_prefix}: {2_sign}{3_value}% para {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_BUILDING', '{1_prefix}: {2_sign}{3_value}% para edificios.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_WONDER', '{1_prefix}: {2_sign}{3_value}% para Maravillas.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_SPACE', '{1_prefix}: {2_sign}{3_value}% para partes de nave espacial.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_SURPLUS', '{1_prefix} Surplus: {2_sign}{3_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_FROM_BUILDINGS', '{1_prefix}: {2_sign}{3_value}% {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_IN_ALL_CITIES', '{1_prefix}: {2_sign}{3_value}% {TXT_KEY_IN_ALL_CITIES}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_STORAGE', '{1_prefix} Almacenado: {3_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_PER_POP', '{1_prefix} por ciudadano [ICON_CITIZEN]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_LAKES', '{1_prefix}: {3_value} Casillas de lago.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_SEA', '{1_prefix}: {3_value} Casillas de mar.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_RIVERS', '{1_prefix}: {3_value} Casillas de rio.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_PLOTS', '{1_prefix}: {3_value} {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_TERRAIN', '{1_prefix}: {3_value} {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_FEATURES', '{1_prefix}: {3_value} {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_RESOURCES', '{1_prefix}: {3_value} {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_TECH', '{1_prefix}: {3_value} con {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_SPECIALISTS', '{1_prefix}: {3_value} {4_extra} en todas las ciudades.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_BUILDINGS', '{1_prefix}: {3_value} para {4_extra}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_FROM_USING_GREAT_PERSON', '{1_prefix} por usar Grandes Personajes: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_YIELD_MOD_HURRY', '{1_prefix} Costes: {2_sign}{3_value}% en todas las ciudades.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_MEDIAN_TECH_PERCENT_CHANGE', '[ICON_RESEARCH] Recompensa por Acuerdo de Investigación: {2_sign}{3_value}%', '', '');
 /* Other */
 
-/* Added by Civup */
-
+/* Agregado por Civup */
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_NOT_FEATURE', 'No mejora [ICON_MINUS] {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_HEAL_RATE_CHANGE', 'Tasa de curación [ICON_PLUS]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_MINOR_FRIENDSHIP_CHANGE', 'Influencia [ICON_INFLUENCE]: +{3_value}% con todas las ciudades-estado.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_MINOR_FRIENDSHIP_FLAT_CHANGE', 'Influencia [ICON_INFLUENCE]: +{3_value} con todas las ciudades-estado.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_REQUIRES_TECH', 'Requiere [ICON_LOCKED]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_CULTURE_FROM_LAKES', 'Cultura [ICON_CULTURE]: {3_value} en casillas de Lago.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_INSTANT_BORDER_RADIUS', 'Expansión de fronteras [ICON_CULTURE]: {3_value} rango de casillas.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_GLOBAL_INSTANT_BORDER_RADIUS', 'Expansión de fronteras [ICON_CULTURE]: {3_value} en todas las ciudades.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_MOUNTAIN_IMPROVEMENT', 'Mejora una montaña [ICON_WORKER]', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_CITY_CAPTURE_CULTURE', 'Cultura [ICON_CULTURE] saqueada de ciudades conquistadas.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_GREAT_GENERAL_RATE_CHANGE', 'Puntos de Gran General [ICON_GREAT_PEOPLE]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_TRADE_DEAL_MODIFIER', 'Recompensas de acuerdos comerciales [ICON_TRADE]: {2_sign}{3_value}%', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_HAPPINESS_INSTANT', 'Puntos instantáneos de Edad de Oro [ICON_GOLDEN_AGE]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_GOLDEN_AGE_POINTS', 'Puntos de Edad de Oro [ICON_GOLDEN_AGE]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_ALREADY_BUILT', 'Ya está construído [ICON_LOCKED]', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_REPLACES', 'Reemplaza [ICON_PLUS]: {3_value}', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-19'), 'TXT_KEY_BUILDING_EFFECT_INSTANT_BORDER_PLOTS', 'Expansion de fronteras [ICON_CULTURE]: {3_value}', '', '');
 /* Expansion */
 
